43aa14a6b348a3dfa91b5f303dfc995437d85ed8
behnazdev <behnaz.mirmiran.jobs@gmail.com>
2025-07-09 12:19:38 +0330

    intial commit - docker-nodejs-api

---
+++ README.md
@@ -0,0 +1,2 @@
+# Node.js API
+یک REST API ساده با Express در Docker.
---
+++ index.js
@@ -0,0 +1,1 @@
+const express = require('express'); const app = express(); app.get('/', (req, res) => res.send('Hello from Node.js API!')); app.listen(3000);
